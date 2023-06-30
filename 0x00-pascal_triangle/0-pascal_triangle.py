bcca2cb699c230ab7caf1f9ab21c5ab9fb30fc71
MikeRock51 <mikerockmusic51@gmail.com>
2023-06-30 05:36:23 +0100

    Add file

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Contains the pascal's traingle solution"""
+
+def pascal_triangle(n):
+    """returns a list of lists of integers representing
+    the Pascal’s triangle of n
+    """
+
