ae939f0191195909699a99bbd79778a961a15b14
MikeRock51 <mikerockmusic51@gmail.com>
2023-06-30 06:05:20 +0100

    Add Solution

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """Contains the pascal's traingle solution"""
 
+
 def pascal_triangle(n):
     """returns a list of lists of integers representing
     the Pascal’s triangle of n
     """
 
+    if n <= 0:
+        return []
+    if n == 1:
+        return [[1]]
+
+    pascal = [[1], [1, 1]]
+    if n == 2:
+        return pascal
+
+    for row in range(1, n - 1):
+        newRow = [1]
+        i = 0
+        while i < len(pascal[row]) - 1:
+            newRow.append(pascal[row][i] + pascal[row][i + 1])
+            i += 1
+        newRow.append(1)
+        pascal.append(newRow)
+
+    return pascal
