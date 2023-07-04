47e884ecb5a96a0bc7efd7913fc8f04b3ade441d
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-04 12:59:20 +0100

    Add Lockbox solution

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,4 +2,25 @@
 """Lockboxes"""
 
 def canUnlockAll(boxes):
+    unusedKeys = []
+    locked = []
 
+    index = 1
+    while index < len(boxes) - 1:
+        currentKeys = boxes[index - 1]
+
+        if index in currentKeys:
+            currentKeys.remove(index)
+        else:
+            locked.append(index)
+        for key in currentKeys:
+            unusedKeys.append(key)
+        index += 1
+
+    if len(locked) == 0:
+        return True
+
+    for index in locked:
+        if not index in unusedKeys:
+            return False
+    return True
