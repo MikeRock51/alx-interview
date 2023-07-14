34c1c020e8df0adaa74483de8ce6c56996179ee2
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-14 06:57:12 +0100

    Update pycodestyle

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """
     In a text file, there is a single character H. Your text editor can
-    execute only two operations in this file: Copy All and Paste. Given a 
+    execute only two operations in this file: Copy All and Paste. Given a
     number n, write a method that calculates the fewest number of
     operations needed to result in exactly n H characters in the file.
 """
+
 
 def isPrime(n: int) -> bool:
     """Checks whether or not n is prime"""
@@ -16,6 +17,7 @@ def isPrime(n: int) -> bool:
         if n % i == 0:
             return False
     return True
+
 
 def minOperations(n: int) -> int:
     """
