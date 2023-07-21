6688fb5f0aeb9d7c69d6eb9ff8e3bbe7d96fbc2f
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-21 04:56:07 +0100

    Add finished log parser

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,30 +2,44 @@
 """Computes metrics and print the statistics of inputs"""
 
 from sys import stdin
-from typing import List
 import re
-
-
-def validateFormat(splitInput: List[str]) -> bool:
-    """Validates if input string is the right format"""
-
-    pass
 
 
 def logParser():
     """Reads each line of stdin and prints statistics"""
 
     inputFormat = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' \
-                     r' - \[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6})\]' \
-                     r' "(\w+ \S+ \S+)"' \
-                     r' (\d{3})' \
-                     r' (\d+)$'
+        r' - \[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6})\]' \
+        r' "(\w+ \S+ \S+)"' \
+        r' (\d{3})' \
+        r' (\d+)$'
+
+    counter = 0
+    statusCodes = {}
+    fileSizeSum = 0
 
     for line in stdin:
-       if re.match(inputFormat, line):
-           print('Matched')
-       else:
-           print('Not Matched')
+        match = re.match(inputFormat, line)
+        if not match:
+            continue
+
+        statusCode = match.group(4)
+        fileSize = match.group(5)
+
+        if statusCode not in statusCodes:
+            statusCodes[statusCode] = 1
+        else:
+            statusCodes[statusCode] += 1
+
+        fileSizeSum += int(fileSize)
+
+        counter += 1
+
+        if not counter % 10:
+            print(f"File size: {fileSizeSum}")
+            for status, count in statusCodes.items():
+                print(f"{status}: {count}")
+
 
 if __name__ == '__main__':
     logParser()
