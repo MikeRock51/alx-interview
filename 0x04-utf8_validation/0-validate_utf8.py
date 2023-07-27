0754adfd234a2d2d38837ce86bb7b2cfd932a27e
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-27 17:41:47 +0100

    Update byte checking

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,27 +4,20 @@
 
 def validUTF8(data):
     """Determines of a given data set represents a valid UTF-8 encoding"""
-    # if type(data) != list or len(data) < 1:
-    #     return False
+    numOfBytes = 1
 
-    # for byte in data:
-    #     if type(byte) != int:
-    #         return False
-
-    byte_count = 0
-
-    for i in data:
-        if byte_count == 0:
-            if i >> 5 == 0b110 or i >> 5 == 0b1110:
-                byte_count = 1
-            elif i >> 4 == 0b1110:
-                byte_count = 2
-            elif i >> 3 == 0b11110:
-                byte_count = 3
-            elif i >> 7 == 0b1:
+    for char in data:
+        if numOfBytes == 1:
+            if char >> 5 in [0b110, 0b1110]:
+                numOfBytes = 2
+            elif char >> 4 == 0b1110:
+                numOfBytes = 3
+            elif char >> 3 == 0b11110:
+                numOfBytes = 4
+            elif char >> 7 == 0b1:
                 return False
         else:
-            if i >> 6 != 0b10:
+            if char >> 6 != 0b10:
                 return False
-            byte_count -= 1
-    return byte_count == 0
+            numOfBytes -= 1
+    return numOfBytes == 1
