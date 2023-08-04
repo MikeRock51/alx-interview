69254e7cf451b4cb309a938e080fd6bce149e8ab
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-04 12:48:14 +0100

    Add N Queens Solution

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -7,30 +7,57 @@
 from sys import argv
 
 
-if len(argv) != 2:
-    print("Usage: nqueens N")
-    exit(1)
+def nQueens(n: int) -> None:
+    """
+        Finds all possible ways to place n queens on an
+        n x n board without them attacking each other
+    """
+    board = []
+    usedCols = set()
+    nDiagonals = set()
+    pDiagonals = set()
 
-try:
-    int(argv[1])
-except Exception:
-    print("N must be a number")
-    exit(1)
+    def backtrack(row: int) -> None:
+        """Prints all possible solution"""
+        if row == n:
+            print(board)
+            return
 
-N = int(argv[1])
+        for col in range(n):
+            if col in usedCols or col - row in nDiagonals\
+                    or col + row in pDiagonals:
+                continue
+            usedCols.add(col)
+            nDiagonals.add(col - row)
+            pDiagonals.add(col + row)
+            board.append([row, col])
 
-if N < 4:
-    print("N must be at least 4")
-    exit(1)
+            backtrack(row + 1)
 
-board = []
+            usedCols.remove(col)
+            nDiagonals.remove(col - row)
+            pDiagonals.remove(col + row)
+            board.remove([row, col])
 
-for i in range(N):
-    row = []
-    for col in range(N):
-        row.append([i, col])
+    backtrack(0)
 
-    board.append(row)
+    # for row in board:
+    #     print(row)
 
-for row in board:
-    print(row)
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+
+    try:
+        N = int(argv[1])
+    except ValueError:
+        print("N must be a number")
+        exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    nQueens(N)
