d55d7a351992280695593fbf9a335fd99b1f7d92
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-31 07:28:39 +0100

    Remove check

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,9 +8,6 @@ def island_perimeter(grid):
     gridWidth = len(grid[0])
     perimeter = 0
 
-    if gridHeight > 100 or gridWidth > 100:
-        return
-
     for i in range(1, gridHeight - 1):
         for j in range(1, gridWidth - 1):
             if grid[i][j] == 1:
