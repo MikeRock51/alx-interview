0c4d34c4c63e1961a740de6993f190595f64f174
MikeRock51 <mikerockmusic51@gmail.com>
2023-09-07 07:57:22 +0100

    Init project

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Prime Game"""
+
+
+def isWinner(x, nums):
+    """Returns the winner of the game"""
+    def isPrime(num):
+        """Returns whether or nor a number is a prime number"""
+        if num <= 1:
+            return false
+        if num <= 
