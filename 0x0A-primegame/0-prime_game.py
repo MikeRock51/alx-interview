28c1ec7faaf7fcc26a7bde11c3a28047e62c77d4
MikeRock51 <mikerockmusic51@gmail.com>
2023-09-07 10:08:48 +0100

    Add solution

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -7,5 +7,46 @@ def isWinner(x, nums):
     def isPrime(num):
         """Returns whether or nor a number is a prime number"""
         if num <= 1:
-            return false
-        if num <= 
+            return False
+        if num <= 3:
+            return True
+        if not num % 2 or not num % 3:
+            return False
+
+        i = 5
+        while (i * i < num):
+            if not num % i or not num % (i + 2):
+                return False
+            i += 6
+
+        return True
+
+    wins = {
+        'Ben': 0,
+        'Maria': 0,
+    }
+
+    for i in range(x):
+        primes = []
+        players = ['Maria', 'Ben']
+
+        for n in range(2, nums[i] + 1):
+            if isPrime(n):
+                primes.append(n)
+
+        # print(f'Primes: {primes}')
+        if len(primes) % 2 != 0:
+            wins['Maria'] += 1
+        else:
+            wins['Ben'] += 1
+
+    if wins['Maria'] == wins['Ben']:
+        return None
+
+    keyMax = max(wins, key=lambda x: wins[x])
+
+    return keyMax
+
+
+if __name__ == '__main__':
+    print("Winner: {}".format(isWinner(3, [4, 5, 1])))
